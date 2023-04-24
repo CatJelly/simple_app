5718442187df4b2f071a36566a3a03705b9b4bbb
ParkJinYong <mej1406@gmail.com>
2023-04-24 13:23:19 +0900

    hello world

--- src/main/java/com/example/simpleapp/SimpleController.java
+++ src/main/java/com/example/simpleapp/SimpleController.java
@@ -3,14 +3,15 @@ package com.example.simpleapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("/")
 @Controller
 public class SimpleController {
 
     @GetMapping("/")
+    @ResponseBody
     public String index() {
-        return "Hello WOrld";
+        return "Hello World";
     }
 
 }
